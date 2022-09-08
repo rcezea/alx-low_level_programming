11a0f30d03eb4bb1a07f3a97a0531b302ba14755
rcezea <rclancing@gmail.com>
2022-09-08 09:19:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Standard library above
+ * Main function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	/**
+	 *return true
+	*/
+	return(0);
+}
