30d532470c6139f442c61ea8bf99890115eb851e
rcezea <rclancing@gmail.com>
2022-09-09 12:12:55 -0700

    combination in business

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
