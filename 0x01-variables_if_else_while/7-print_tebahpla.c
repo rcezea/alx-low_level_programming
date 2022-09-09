ef2bafef9d10b6882dcb329cc5c8eff585770c31
rcezea <rclancing@gmail.com>
2022-09-09 05:07:35 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
