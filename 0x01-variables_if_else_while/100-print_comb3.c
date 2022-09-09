0448c40142761ccef5248cb422d83536d699c082
rcezea <rclancing@gmail.com>
2022-09-09 06:29:09 -0700

    combination of brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if ( i == 8 && j == 9 )
+				continue;
+			putchar(44);
+			putchar(32);
+			
+		}
+		putchar(10);
+	}
+	return (0);
+}
