28e0431f5f9a2152484da0444c567031b211baaa
rcezea <rclancing@gmail.com>
2022-09-09 03:32:01 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int i = 97, a = 10;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(a);
+	return (0);
+}
