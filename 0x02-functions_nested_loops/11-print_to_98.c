527d5b3074e4bd16c78a2f53dd4ee2518f3776ea
rcezea <rclancing@gmail.com>
2022-09-13 13:58:33 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,17 @@ void print_to_98(int n)
 	{
 		while (n > 97)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			if (n == 98)
-				break;
+			if (n > 99)
+			{
+				_putchar((n / 100) + '0');
+				_putchar(((n % 100) / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if (n < 100)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 			_putchar(44);
 			_putchar(32);
 			n--;
@@ -21,17 +28,22 @@ void print_to_98(int n)
 	}
 	else if (n <= 98)
 	{
-
 		while (n < 99)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			if (n == 98)
-				break;
+			if (n < 10)
+			{
+				_putchar(n + '0');
+			}
+			else if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 			_putchar(44);
 			_putchar(32);
 			n++;
 		}
 	}
-	_putchar(10);
+	putchar(57);
+	putchar(56);
 }
