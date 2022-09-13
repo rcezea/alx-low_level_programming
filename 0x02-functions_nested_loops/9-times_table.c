b80a2c5889a4c21f19cc1c39db293b0dea9e747e
rcezea <rclancing@gmail.com>
2022-09-13 07:05:10 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,15 +4,18 @@
  */
 void times_table(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i = 0, j = 0;
+	while (i < 10)
 	{
-		for (int j = 0; j < 10; j++)
+		while (j < 10)
 		{
 			_putchar(i * j);
 			if (j == 9)
 				continue;
 			_putchar(44);
 			_putchar(32);
+			j++;
 		}
+		i++;
 	}
 }
