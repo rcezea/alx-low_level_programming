d2baa30f92ec3d829af25fc7c6235fdda8885b15
rcezea <rclancing@gmail.com>
2022-09-13 14:44:21 -0700

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 0, sum = 0;;
 
 	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("%d", i);
+			sum += i;
 		}
 		i++;
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return (0);
 }
