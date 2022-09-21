ecc883c4d5d516dbd3f3e91f7f558edc71133017
rcezea <rclancing@gmail.com>
2022-09-21 17:04:51 +0100

    Advanced tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string into 1337x
+ * @str: string passed
+ * Return: encoded string
+ */
+ char *rot13(char *str)
+ {
+     int i = 0, j = 0;
+     char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+     char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+     while(str[i])
+     {
+         j = 0;
+         while(alpha[j])
+         {
+             if (str[i] == alpha[j])
+             {
+                 str[i] = rot[j];
+                 break;
+             }
+
+             j++;
+         }
+         i++;
+     }
+     return (str);
+ }
+
