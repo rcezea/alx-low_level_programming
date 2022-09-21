ec6eb8682319f5e6f8eab1f9030f135c9c0e8d51
rcezea <rclancing@gmail.com>
2022-09-21 17:12:24 +0100

    Mandatory tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,19 +6,19 @@
  * Return: dest
  */
  char *_strcat(char *dest, char *src)
- {
-     int i = 0, j = 0;
+{
+    int i = 0, j = 0;
 
-     while (dest[i])
-     {
-         i++;
-     }
+    while (dest[i])
+    {
+     i++;
+    }
 
-     while (src[j] != '\0')
-     {
-         dest[i] = src[j];
-         i++;
-         j++;
-     }
-     return (dest);
- }
+    while (src[j] != '\0')
+    {
+     dest[i] = src[j];
+     i++;
+     j++;
+    }
+    return (dest);
+}
