f45809387189f536114b6cb3b3ef233401ffc6de
rcezea <rclancing@gmail.com>
2022-09-21 11:03:31 -0700

    Advanced tasks

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,27 +4,26 @@
  * @str: string passed
  * Return: encoded string
  */
- char *rot13(char *str)
- {
-     int i = 0, j = 0;
-     char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-     char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char *rot13(char *str)
+{
+	int i = 0, j = 0;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-     while(str[i])
-     {
-         j = 0;
-         while(alpha[j])
-         {
-             if (str[i] == alpha[j])
-             {
-                 str[i] = rot[j];
-                 break;
-             }
-
-             j++;
-         }
-         i++;
-     }
-     return (str);
- }
+	while (str[i])
+	{
+		j = 0;
+		while (alpha[j])
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
 
