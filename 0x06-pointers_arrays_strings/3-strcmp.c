cd7d344298835010ebd928dd5020cffd0fa024b7
rcezea <rclancing@gmail.com>
2022-09-21 11:18:21 -0700

    Mandatory tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
+	int i = 0;
+
+	while (s1[i] || s2[i])
 	{
-		return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	else if (*s1 > *s2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (-15);
-	}
+	return (0);
 }
