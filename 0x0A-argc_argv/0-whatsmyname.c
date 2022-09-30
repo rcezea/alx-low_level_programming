b7a7a9ce37e3bd79b6cf7dd1fd15198b099f4540
rcezea <rclancing@gmail.com>
2022-09-30 17:08:49 +0100

    Mandatory task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * Main - Entry point
+ *
+ * @argc: size of array argv / number of arguments
+ *
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
