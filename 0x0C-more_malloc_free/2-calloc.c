a6c3de99c6a830d09793c755a35dcdda2cc2f772
rcezea <rclancing@gmail.com>
2022-10-06 15:56:42 +0100

    Mandatory task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc((nmemb * size) + 1);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 	char_cast = arr;
