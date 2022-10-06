043f3c91cad8bf5138331b34ba8eb8fa24978570
rcezea <rclancing@gmail.com>
2022-10-06 15:48:45 +0100

    Mandatory task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * void *_calloc(unsigned int nmemb, unsigned int size) - allocates memory for an array
+ * @nmemb: number of member elements
+ * @size: size of element (data type)
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	char *char_cast;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc((nmemb * size) + 1);
+	if (arr == NULL)
+		return (NULL);
+	char_cast = arr;
+	while (i < (size * nmemb))
+	{
+		char_cast[i] = '\0';
+		i++;
+	}
+	return (arr);
+}
