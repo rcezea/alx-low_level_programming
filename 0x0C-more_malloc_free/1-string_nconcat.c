ca5e360edf58e3872c7c3606634b9c7cea08a00c
rcezea <rclancing@gmail.com>
2022-10-06 15:01:06 +0100

    Mandatory task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *string;
 	int count = n;
-	int i = 0, j = 0;
+	int i = 0, j = 0, size;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -21,17 +21,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		i++;
 	}
-	count = count + i + 1;
-	i = 0;
-	string = malloc(count);
+	size = count + i + 1;
+	string = malloc(size);
 	if (string == NULL)
 		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
 		string[i] = s1[i];
 		i++;
 	}
-	while (i < count)
+	while (j < count)
 	{
 		string[i] = s2[j];
 		i++;
