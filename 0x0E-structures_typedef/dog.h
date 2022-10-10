492443a3fc6a08df5ae149d4812191e99e3fd86e
rcezea <rclancing@gmail.com>
2022-10-11 00:11:12 +0100

    Mandatory task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - properties of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
