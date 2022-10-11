7e6496a092373d6d2bfd4483b2df9ee0b9aa4e28
rcezea <rclancing@gmail.com>
2022-10-10 17:33:58 -0700

    Mandatory task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include "dog.h"
+
 /**
  * print_dog - initializes a variable of type struct dog
  * @d: dog struct
  */
+
 void print_dog(struct dog *d)
 {
 	if (d)
@@ -13,9 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-
 		printf("Age: %.6f\n", d->age);
-
 		if (d->owner)
 			printf("Name: %s\n", d->owner);
 		else
