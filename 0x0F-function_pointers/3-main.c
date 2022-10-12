4f0085b51cb0d91e0670f2789307171ad138b869
rcezea <rclancing@gmail.com>
2022-10-12 19:59:46 +0100

    Mandatory task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc - number of arguments
+ * @argv - array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    int a,b;
+    char *op;
+
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+    op = argv[2];
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+		printf("%d\n", get_op_func(op)(a, b));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - finds the sum of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
+/**
+ * op_sub - finds the difference b/w two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	int diff = a - b;
+	return (diff);
+}
+/**
+ * op_mul - finds the product of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	int prod = a * b;
+	return (prod);
+}
+/**
+ * op_div - finds the quotient of two integers
+ * @a: integer
+ * @b: divisor
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	int quotient = a / b;
+	return (quotient);
+}
+/**
+ * op_mod - finds the modulus of an integer
+ * @a: integer
+ * @b: mod
+ * Return: modulus value
+ */
+int op_mod(int a, int b)
+{
+	int modulus = a % b;
+	return (modulus);
+}
