c31e99a6ff5b8580e873f348cc25b46c6ca97ddb
rcezea <rclancing@gmail.com>
2022-10-12 15:04:54 -0700

    Mandatory task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,25 +8,27 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
+	char *op;
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	op = argv[2];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(argv[2]) == NULL || argv[2] != '\0')
+	if (get_op_func(op) == NULL || *op == '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' && b == 0) || (argv[2] == '%' && b == 0))
+	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(a, b));
+	printf("%d\n", get_op_func(op)(a, b));
 
 	return (0);
 }
