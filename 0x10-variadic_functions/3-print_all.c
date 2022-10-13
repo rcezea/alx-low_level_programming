c7dbcdca667896f153595520ef19e3bf6422f8ff
rcezea <rclancing@gmail.com>
2022-10-13 13:43:34 +0100

    Mandatory task

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments
+ */
+void print_all(const char * const format, ...)
+{
+	va_list all;
+
+}
