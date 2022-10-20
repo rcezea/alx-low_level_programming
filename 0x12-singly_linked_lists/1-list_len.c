ae610ebd31a3ff28d4127613ed568c050ff79ac5
rcezea <rclancing@gmail.com>
2022-10-20 20:51:41 +0100

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: linked list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
