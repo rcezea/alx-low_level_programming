99a493d2489edb527bf61d971dd2d544ffd98051
rcezea <rclancing@gmail.com>
2022-10-20 20:38:25 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,9 +10,9 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while (h->str != NULL)
+	while (h)
 	{
-		if (h != NULL)
+		if (h->str != NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
