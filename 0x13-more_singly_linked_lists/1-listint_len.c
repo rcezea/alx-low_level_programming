10def343c5de1ac97cd83f0c99993fd1c32529c9
rcezea <rclancing@gmail.com>
2022-10-24 17:38:09 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
- * listint-len - gets number of elements
+ * listint_len - gets number of elements
  * @h: linked list
  * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		i++;
+		nodes++;
 		h = h->next;
 	}
-	return (i);
+	return (nodes);
 }
