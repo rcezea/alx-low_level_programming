7924bfe6f194b273bb80eb0fdb82b61b8dda680d
rcezea <rclancing@gmail.com>
2022-10-24 17:39:13 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - geats sum of all the data
+ * @head: pointer to head node
+ * Return: sum of all data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
