b05d7b22ec53b3b297f7cb90500a01661e2144de
Richard <rclancing@gmail.com>
2023-01-12 05:53:05 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,72 +1,53 @@
 #include "hash_tables.h"
-#include "string.h"
-#include <stdlib.h>
-#include <stdio.h>
-/**
- * hash_table_set - setting key ans value pairs in the hash table
- * @ht: hash_table
- * @key: key
- * @value: value
- * Return: 1 if success else 0
- */
 
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index;
-	hash_node_t *h_node, *tmp_node;
-	char *dup_value;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (strlen(key) == 0 || ht == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	tmp_node = (ht->array)[index];
-	while (tmp_node)
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(tmp_node->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			dup_value = strdup(value);
-			if (!dup_value)
-				return (0);
-			free(tmp_node->value);
-			tmp_node->value = dup_value;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
-		tmp_node = tmp_node->next;
 	}
-	h_node = get_node((char *)key, (char *)value);
-	if (!h_node)
-		return (0);
-	h_node->next = (ht->array)[index];
-	ht->array[index] = h_node;
-	return (1);
-}
 
-/**
- * get_node - create the key-value pair as a list
- * @key: key
- * @value: value
- * Return: address of node else null
- */
-
-hash_node_t *get_node(char *key, char *value)
-{
-	hash_node_t *h_node;
-	char *dup_key, *dup_value;
-
-	dup_key = strdup(key);
-	if (dup_key == NULL)
-		return (NULL);
-	dup_value = strdup(value);
-	if (dup_value == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(dup_key);
-		return (NULL);
+		free(value_copy);
+		return (0);
 	}
-	h_node = malloc(sizeof(hash_node_t));
-	if (!h_node)
-		return (NULL);
-	h_node->key = dup_key;
-	h_node->value = dup_value;
-	h_node->next = NULL;
-	return (h_node);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
