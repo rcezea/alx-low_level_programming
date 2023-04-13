b2e19bc7decbb464caa55f342e4417b5222976a5
rcezea <rclancing@gmail.com>
2023-04-13 21:26:31 +0100

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef _SEARCH_ALGOS_H
-#define _SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
