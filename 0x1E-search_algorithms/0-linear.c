d4bea54c891bd860c253e12b681a7d68bae2b672
rcezea <rclancing@gmail.com>
2023-04-13 16:45:38 +0100

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * linear_search - uses linear search algo
+ * @array: array to search
+ * @size: size of array to search
+ * @value: value to search for
+ * Return: return index if sucessfull else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+    printf("Musty");
+	return (-1);
+}
